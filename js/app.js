5e35dc7fffcdf10d354b8f582eff67bedde51f44
Noel Garcia Guimeras <garcia.guimeras@gmail.com>
2020-04-18 14:21:44 -0400

    Changing sw load path

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
 
-    navigator.serviceWorker.register('./sw.js').then(
+    navigator.serviceWorker.register('/sw.js').then(
       function(registration) {
         console.log('SW register: OK');
         console.log(registration);
